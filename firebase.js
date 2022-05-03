9876cb8f03be0c1b17d4eaee8a65b7167e67d774
Julian Villada <a@gmail.com>
2022-05-03 15:49:18 -0500

    se agrega firebase

---
+++ firebase.js
@@ -0,0 +1,22 @@
+  /*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAx3xWMgA9aGjalIATT-3pJS3yhRep35gw",
+    authDomain: "latiendita-5e7b0.firebaseapp.com",
+    projectId: "latiendita-5e7b0",
+    storageBucket: "latiendita-5e7b0.appspot.com",
+    messagingSenderId: "31783758474",
+    appId: "1:31783758474:web:8732a8bf675d7867e45b73",
+    measurementId: "G-ZBL1W3J2W7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
