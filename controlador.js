fb984d6aa80588337b7b12fc9fba90d13ccae41f
Julian Villada <a@gmail.com>
2022-05-11 17:07:57 -0500

    se agrega bootstrap

--- controlador.js
+++ controlador.js
@@ -166,6 +166,22 @@ let contenedor=document.getElementById("contenedor")
 
 //RECORRO EL ARREGLO
 productos.forEach(function(producto){
-    console.log(producto.nombre)
+    let foto=document.createElement("img")
+    foto.classList.add("w-100" , "img-fluid")
+    foto.src=producto.foto
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+
+
+
+
+
+
 })
 
