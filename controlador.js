199ec7736c89f946ffa953cbabf0d9eecd7deeb2
Julian Villada <your@gmail.com>
2022-05-12 17:14:28 -0500

    se agrega popularidad y pais

--- controlador.js
+++ controlador.js
@@ -3,6 +3,8 @@ let producto1={
     nombre:"8 GB de ram",
     precio:450000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐☆",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/8%20GB%20de%20ram.webp?alt=media&token=8c2c7f0f-ff9a-42b6-827a-063e3f28e787"
 }
 
@@ -10,6 +12,8 @@ let producto2={
     nombre:"audifonos",
     precio:160000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "China",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/audifonos.webp?alt=media&token=00d89590-4d33-41ef-bf22-ecfc5904e3a6"
 }
 
@@ -17,13 +21,17 @@ let producto3={
     nombre:"bicicleta",
     precio:600000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/bicicleta.webp?alt=media&token=ed01f347-30d0-4e31-8e35-9602f522e94c"
 }
 
 let producto4={
     nombre:"casco para bicicleta",
     precio:50000,
-    estado:true,
+    estado:false,
+    popularidad: "⭐⭐⭐☆☆",
+    pais: "Colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/casco%20para%20bicicleta.webp?alt=media&token=30479b36-3bfc-41bf-ab73-7a3359f5e7ee"
 }
 
@@ -31,6 +39,8 @@ let producto5={
     nombre:"gafas",
     precio:10000,
     estado:true,
+    popularidad: "⭐⭐☆☆☆",
+    pais: "Canadà",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/gafas.webp?alt=media&token=cecc846f-8d30-49f6-857a-a069c1b52057"
 }
 
@@ -38,13 +48,17 @@ let producto6={
     nombre:"peluche gnar league of legends",
     precio: 75000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/gnar%20peluche.webp?alt=media&token=262ce087-97d2-44e8-b864-8a5a2d2cdc17"
 }
 
 let producto7={
     nombre:"lavadora",
     precio:1000000,
-    estado:true,
+    estado:false,
+    popularidad: "⭐⭐⭐⭐☆",
+    pais: "Colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/lavadora.webp?alt=media&token=f90bb37c-3cda-4359-ba68-c14d15476332"
 }
 
@@ -52,13 +66,17 @@ let producto8={
     nombre:"limpiador facial",
     precio:22000,
     estado:true,
+    popularidad: "⭐⭐⭐☆☆",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/limpiador%20facial.webp?alt=media&token=0ffa7f98-fe3c-49a0-b433-e38cfb6d7322"
 }
 
 let producto9={
     nombre:"oso de peluche",
     precio:76000,
-    estado:true,
+    estado:false,
+    popularidad: "⭐⭐⭐⭐☆",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/oso%20de%20peluche.webp?alt=media&token=4aaabdc9-0f97-4049-9769-bf771ef3e378"
 }
 
@@ -66,6 +84,8 @@ let producto10={
     nombre:"parrilla asador de acero y aluminio portable camping",
     precio:70000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "Canadà",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/parrilla%20asador%20de%20acero%20y%20aluminio%20portable%20camping.webp?alt=media&token=8a58fd1b-43eb-4278-8354-963f3ab6e411"
 }
 
@@ -73,6 +93,8 @@ let producto11={
     nombre:"pc gamer",
     precio:600000,
     estado:true,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "España",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/pc%20gamer.webp?alt=media&token=47a668ef-9d2b-430a-8070-f95cb2dcd6e8"
 }
 
@@ -80,6 +102,8 @@ let producto12={
     nombre:"pesa",
     precio:80000,
     estado:true,
+    popularidad: "⭐⭐☆☆☆",
+    pais: "China",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/pesa.webp?alt=media&token=299188a6-b0d8-4a46-bb54-4102d0d45e12"
 }
 
@@ -87,13 +111,17 @@ let producto13={
     nombre:"maleta de viaje",
     precio:60000,
     estado:true,
+    popularidad: "⭐☆☆☆☆",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/maleta%20de%20viaje.webp?alt=media&token=6dfd77ab-db30-4310-b61d-18a2f5a26139"
 }
 
 let producto14={
     nombre:"microhondas",
     precio:120000,
-    estado:true,
+    estado:false,
+    popularidad: "⭐⭐⭐☆☆",
+    pais: "USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/microhondas.webp?alt=media&token=8f6d379c-8bba-45ed-befd-5b2a3aed63a2"
 }
 
@@ -101,42 +129,54 @@ let producto15={
     nombre:"mochila",
     precio:21000,
     estado:true,
+    popularidad: "⭐☆☆☆☆",
+    pais: "Colombia",
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/mochila.webp?alt=media&token=8f242c3f-1935-4a0d-828b-ad797a83e3ec"
 }
 
 let producto16={
-    nombre:"producto16",
+    nombre:"purificador de agua",
     precio:500000,
     estado:true,
-    foto:""
+    popularidad: "⭐☆☆☆☆",
+    pais: "USA",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/purificador%20de%20agua.webp?alt=media&token=c93468a3-3aa6-4859-afcd-422ca889d62e"
 }
 
 let producto17={
-    nombre:"producto17",
-    precio:500000,
+    nombre:"xiaomi",
+    precio:800000,
     estado:true,
-    foto:""
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "Colombia",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/xiaomi.webp?alt=media&token=51455e64-ab9a-4973-9eec-1a0b9a1e144b"
 }
 
 let producto18={
-    nombre:"producto18",
-    precio:500000,
-    estado:true,
-    foto:""
+    nombre:"sillon de oficina",
+    precio:2000000,
+    estado:false,
+    popularidad: "⭐⭐⭐⭐☆",
+    pais: "China",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/sillon%20de%20oficina.webp?alt=media&token=92c53867-d66f-4068-b789-053a441ae88b"
 }
 
 let producto19={
-    nombre:"producto19",
-    precio:500000,
-    estado:true,
-    foto:""
+    nombre:"tableta",
+    precio:560000,
+    estado:true ,
+    popularidad: "⭐⭐⭐⭐⭐",
+    pais: "USA",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/tableta.webp?alt=media&token=b38c0f78-eb85-4285-8af2-d2b5fc685575"
 }
 
 let producto20={
-    nombre:"producto20",
-    precio:500000,
-    estado:true,
-    foto:""
+    nombre:"nevera",
+    precio:2000000,
+    estado:false,
+    popularidad: "⭐⭐⭐⭐☆",
+    pais: "Colombia",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/nevera.webp?alt=media&token=94c1f59b-387a-4f17-bb0f-700c43f70aca"
 }
 
 //ARREGLO
@@ -160,7 +200,7 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20
+    producto20,
 )
 let contenedor=document.getElementById("contenedor")
 
@@ -172,8 +212,36 @@ productos.forEach(function(producto){
     let columna=document.createElement("div")
     columna.classList.add("col")
     let tarjeta=document.createElement("div")
+    let nombreProductos=document.createElement("h5")
+    nombreProductos.classList.add("text-center")
+    nombreProductos.textContent=producto.nombre
     tarjeta.classList.add("card")
+    let precio=document.createElement("h5")
+    precio.classList.add("text-center")
+    precio.classList.add("bg-success")
+    precio.textContent="$"+producto.precio
+    let popularidad=document.createElement("h5")
+    popularidad.classList.add("text-center")
+    popularidad.classList.add("bg-dark")
+    popularidad.classList.add("text-white")
+    popularidad.textContent=producto.popularidad
+    let pais=document.createElement("h5")
+    pais.classList.add("text-center")
+    pais.textContent=producto.pais
+    let estado=document.createElement("h5")
+    if (producto.estado==true){
+        estado.classList.add("text-center")
+        estado.textContent="Nuevo"
+    }else{
+        estado.classList.add("text-center")
+        estado.textContent="Usado"
+    }
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProductos)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(estado)
+    tarjeta.appendChild(popularidad)
+    tarjeta.appendChild(pais)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
 
