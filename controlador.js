a34ef4bdf815e64f97221e1af2a23086aa12f8c5
Julian Villada <your@gmail.com>
2022-05-17 16:58:49 -0500

    se continua pagina web de tiendas

--- controlador.js
+++ controlador.js
@@ -179,6 +179,278 @@ let producto20={
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/nevera.webp?alt=media&token=94c1f59b-387a-4f17-bb0f-700c43f70aca"
 }
 
+let producto21={
+    nombre:"alfombra circular" ,
+    precio:200000 ,
+    estado: true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/alfombra%20circular.webp?alt=media&token=a951928f-d618-4ffb-9594-d97fef40c94d"
+}
+
+let producto22={
+    nombre:"armaf club de nuit intense edt 105 ml para hombre" ,
+    precio: 350000 ,
+    estado: true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/armaf%20club%20de%20nuit%20intense%20edt%20105%20ml%20para%20hombre.webp?alt=media&token=ef79dcc0-0aa7-4413-9494-735cc6a49e88"
+}
+
+let producto23={
+    nombre:"baraja de cartas para magia" ,
+    precio: 15000 ,
+    estado: false ,
+    popularidad:"⭐⭐⭐☆☆" ,
+    pais:"China" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/barajas%20de%20cartas%20para%20magia.webp?alt=media&token=913e5c21-6519-4569-9af2-56ed8720ca76"
+}
+
+let producto24={
+    nombre:"cama" ,
+    precio:700000 ,
+    estado:false ,
+    popularidad: "⭐⭐☆☆☆" ,
+    pais: "Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cama.webp?alt=media&token=caa5da8e-dd7c-40cc-a27c-c7c5e8f9144e"
+}
+
+let producto25={
+    nombre:"camara web 1080p para pc laptop usb web cam con microfono" ,
+    precio: 200000 ,
+    estado: true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Canadà" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/camara%20web%201080p%20para%20pc%20laptop%20usb%20web%20cam%20con%20microfono.webp?alt=media&token=b7ddae4b-617e-4514-81cd-ba32ef4d3719"
+}
+
+let producto26={
+    nombre:"Carro control remoto" ,
+    precio:500000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/carro%20control%20remoto.webp?alt=media&token=d5d86838-9a65-4cd8-96bb-862440fb30f8"
+}
+
+let producto27={
+    nombre:"carro montable de juguete" ,
+    precio: 1000000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"España" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/carro%20montable%20de%20juguete.webp?alt=media&token=1819d3a4-faee-4b89-aaf9-bb79aabcc043"
+}
+
+let producto28={
+    nombre:"cinta de silicona" ,
+    precio: 24500 ,
+    estado: true ,
+    popularidad:"⭐⭐☆☆☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cinta%20de%20silicona.webp?alt=media&token=b7cf1398-063c-464f-bdd3-cde67f918811"
+}
+
+let producto29={
+    nombre:"crocs" ,
+    precio:30000 ,
+    estado: true ,
+    popularidad:"⭐⭐⭐☆☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/crocs.webp?alt=media&token=93e32ccd-38b8-408b-a78d-c11bd81e7bac"
+}
+
+let producto30={
+    nombre:"cubo 3x3" ,
+    precio: 28000 ,
+    estado: true ,
+    popularidad: "⭐⭐⭐⭐⭐" ,
+    pais: "USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cubo%203x3.webp?alt=media&token=25173d9d-6fb6-4e59-8c59-f39b7dd6de03"
+}
+
+let producto31={
+    nombre:"cuchillo de supervivencia" ,
+    precio:60000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Canadà" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cuchillo%20de%20supervivencia.webp?alt=media&token=4d06b850-7794-4783-966e-c55861a32b36"
+}
+
+let producto32={
+    nombre:"cuchillo de mariposa csgo" ,
+    precio:20000 ,
+    estado:false ,
+    popularidad:"⭐⭐⭐☆☆" ,
+    pais:"Canadà" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cuchillo%20mariposa%20csgo.webp?alt=media&token=57ad4c65-5c76-4879-9b9f-d331d4f0d1be"
+}
+
+let producto33={
+    nombre:"cuchillo tactico csgo" ,
+    precio:200000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/cuchillo%20tactico%20csgo.webp?alt=media&token=82b99d8c-f816-4a62-ba42-510f69000d91"
+}
+
+let producto34={
+    nombre:"daga medieval para coleccionistas" ,
+    precio: 300000 ,
+    estado: false ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"China" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/daga%20medieval%20para%20coleccionistas.webp?alt=media&token=fe03d190-3703-4faa-9683-74fe00576417" ,
+
+}
+
+let producto35={
+    nombre:"gato egipcio estatua" ,
+    precio:350000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"Egipto" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/gato%20egipcio%20estatua.webp?alt=media&token=72b09bb4-d7e6-4cd6-ad91-db77690f0c95"
+}
+
+let producto36={
+    nombre:"hacha tomahawk" ,
+    precio:210000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/hacha%20tomahawk.webp?alt=media&token=3c9b771e-cde2-4572-8c48-4c516bbed9e0"
+}
+
+let producto37={
+    nombre:"helicoptero con control remoto" ,
+    precio:230000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/helicoptero%20con%20control%20remoto.webp?alt=media&token=87e013ea-f6c4-4166-8677-6c3cb11b4922"
+}
+
+let producto38={
+    nombre:"hotwheels garage extremo" ,
+    precio:190000 ,
+    estado:false ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/hotwheels%20garage%20extremo.webp?alt=media&token=a02db258-ec37-46cd-b222-d3e0efca948c"
+}
+
+let producto39={
+    nombre:"carros hotwheels" ,
+    precio:57000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/hotwheels.webp?alt=media&token=719dcbbc-6589-4eb7-80a7-1f3872c4bcc3"
+}
+
+let producto40={
+    nombre:"kit de herramientas" ,
+    precio:120000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/kit%20de%20herramientas.webp?alt=media&token=914ad2aa-b6c2-4717-9b46-c7dda983e40b"
+}
+
+let producto41={
+    nombre:"martillo" ,
+    precio:22000 ,
+    estado:false ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/martillo.webp?alt=media&token=32ce1127-43ce-4595-9fcd-0773ffd74a64" ,
+
+}
+
+let producto42={
+    nombre:"mazo de goma" ,
+    precio:25000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/mazo%20de%20goma.webp?alt=media&token=9033a4fc-84b5-477f-bd6d-3e526f5f1f55"
+}
+
+let producto43={
+    nombre:"nerf alphastrike hammerstorm" ,
+    precio:54000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/nerf%20alphastrike%20hammerstorm.webp?alt=media&token=ec15074b-f5a4-4b59-b79a-80de863538d8"
+}
+
+let producto44={
+    nombre:"pantuflas del rayo mcqueen" ,
+    precio:60000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/pantuflas%20del%20rayo%20mcqueen.webp?alt=media&token=6744fbf8-bb56-4490-8607-83f92b798c87"
+}
+
+let producto45={
+    nombre:"peluche deadpool" ,
+    precio:70000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐☆" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/peluche%20deadpool%20marvel.webp?alt=media&token=9e91ea2a-61c6-4953-bfdd-a06c66ce0d65"
+}
+
+let producto46={
+    nombre:"pico de diamante minecraft" ,
+    precio:86000 ,
+    estado:false ,
+    popularidad:"⭐⭐⭐☆☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/pico%20de%20diamante%20minecraft.webp?alt=media&token=5ff0da19-d687-41cc-ae48-f94039857082"
+}
+
+let producto47={
+    nombre:"play station 4" ,
+    precio:500000 ,
+    estado:false ,
+    popularidad:"⭐⭐☆☆☆" ,
+    pais:"Colombia" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/ps4.webp?alt=media&token=2939df91-f12f-4af8-b94b-9c21b3b0ddd1"
+}
+
+let producto48={
+    nombre:"silla gamer" ,
+    precio: 1200000 ,
+    estado: true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"China" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/silla%20gamer.webp?alt=media&token=97a5b4fa-70c2-4760-8d2b-eb2ee04fefae" ,
+
+}
+
+let producto49={
+    nombre:"silla puff color azul" ,
+    precio:200000 ,
+    estado:true ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"Canadà" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/silla%20puff%20color%20negro.webp?alt=media&token=dab1d76b-c63a-4c8a-b50a-5943419501e9"
+}
+
+let producto50={
+    nombre:"teclado gamer" ,
+    precio: 195000 ,
+    estado: false ,
+    popularidad:"⭐⭐⭐⭐⭐" ,
+    pais:"USA" ,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-5e7b0.appspot.com/o/teclado%20gamer.webp?alt=media&token=841228b4-8e92-4dcb-921d-0641045442cc"
+}
 //ARREGLO
 let productos=Array(
     producto1,
@@ -201,6 +473,36 @@ let productos=Array(
     producto18,
     producto19,
     producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
 )
 let contenedor=document.getElementById("contenedor")
 
@@ -215,7 +517,7 @@ productos.forEach(function(producto){
     let nombreProductos=document.createElement("h5")
     nombreProductos.classList.add("text-center")
     nombreProductos.textContent=producto.nombre
-    tarjeta.classList.add("card")
+    tarjeta.classList.add("card","h-100","shadow")
     let precio=document.createElement("h5")
     precio.classList.add("text-center")
     precio.classList.add("bg-success")
